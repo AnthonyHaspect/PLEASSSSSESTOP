67da48ec3dd430372a6c57bfb91c7129debeb6fe
AnthonyH <2027425@cegepoutaouais.qc.ca>
2023-02-03 11:23:43 -0500

    3em dépot presque fini premier bouton

--- app/src/main/java/com/anthony2027425/pleasssssestop/MainActivity.java
+++ app/src/main/java/com/anthony2027425/pleasssssestop/MainActivity.java
@@ -2,7 +2,7 @@ package com.anthony2027425.pleasssssestop;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
     TextView tvTotalST;
     TextView tvTotalPB;
 
+    public double CalTxP(float val) {
+        return val * 1.05;
+        }
+    public double CalTxF(float val){
+        return val * 1.09975;
+    }
+    public double CalculTotal(double a, double b, float c){
+        return a + b + c;
+    }
 
 
     @Override
@@ -41,19 +50,16 @@ public class MainActivity extends AppCompatActivity {
         tvTotalTPS = findViewById(R.id.tvTotalTPS);
         tvTotalTVQ = findViewById(R.id.tvTotalTVQ);
 
-        public int cacTxTT(int val){
-            double value = val * 1.09975;
-            value +=  val * 1.05;
-            return value;
-
-        }
-
-        btnCalculTx.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("MonLog", "Btn cliquer");
-
-            }
+        Log.d("MonLog", "une ligne avant le bouton");
+        btnCalculTx.setOnClickListener(v -> {
+            Log.d("MonLog", "Btn calcul tx cliquer");
+            float Total = Integer.parseInt(String.valueOf(etSousTot.getText()));
+            double txQueb =  CalTxP(Total);
+            double txFed = CalTxF(Total);
+            double TotalFinal = CalculTotal(txFed, txQueb, Total);
+            tvTotalST.setText((int) TotalFinal);
+            tvTotalTPS.setText((int) txFed);
+            tvTotalTVQ.setText((int) txQueb);
         });
     }
 }
